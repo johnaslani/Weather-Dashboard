644da8fd29da0550c67cc9012528dea57d3f60eb
johnaslani <aslani.john@gmail.com>
2022-08-03 08:25:42 -0700

    Added second API call for UV Index, display function, history button

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,33 +1,85 @@
-var APIKey =5c524c69a77c6b72f3b7282bee372d1d
+var APIKey = "5c524c69a77c6b72f3b7282bee372d1d";
 var city;
-
-//to create input for city name
-
-//URL associated with Current Weather Data.
-
-//api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
 
 // accept user input, to store in the city variable that you've created.
 
+var cityList = document.querySelector("#cityHistory");
+var searchButton = document.getElementById("search-button");
+var searchInput = document.getElementById("search-input");
+var temp = document.getElementById("temp");
+var wind = document.getElementById("wind");
+var humidity = document.getElementById("humidity");
+var uvIndex = document.getElementById("uvIndex");
 
-var repoList = document.querySelector("ul");
-var fetchButton = document.getElementById("fetch-button");
+function getApi(event) {
+  event.preventDefault();
+  city = searchInput.value;
 
-function getApi() {
-  // replace `octocat` with anyone else's GitHub username
-  var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.createElement("li");
-        listItem.textContent = data[i].html_url;
-        repoList.appendChild(listItem);
-      }
+  //URL associated with Current Weather Data
+  // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+  //api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
+  var requestUrl =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  fetch(requestUrl).then(function (response) {
+    console.log(response);
+    if (!response.ok) {
+      console.log("Please select a city name");
+      return;
+    }
+    // displayWeather(response.json());
+    response.json().then((data) => {
+      createButton(data);
+      getWeather(data);
     });
+  });
 }
 
-fetchButton.addEventListener("click", getApi);
+function getWeather(data) {
+  var requestUrl =
+    "http://api.openweathermap.org/data/2.5/onecall?lat=" +
+    data.coord.lat +
+    "&lon=" +
+    data.coord.lon +
+    "&units=imperial&appid=" +
+    APIKey;
+  fetch(requestUrl).then(function (response) {
+    console.log(response);
+    if (!response.ok) {
+      console.log("Please select a city name");
+      return;
+    }
+    // displayWeather(response.json());
+    response.json().then((data) => {
+      displayWeather(data);
+    });
+  });
+}
+
+function displayWeather(data) {
+  console.log(data);
+  const weather = data.current;
+  temp.textContent = weather.temp;
+  humidity.textContent = weather.humidity;
+  wind.textContent = weather.wind_speed;
+  uvIndex.textContent = weather.uvi;
+}
+
+function createButton(data) {
+  // for (var i = 0; i < data.length; i++) {
+  var listItem = document.createElement("button");
+  //listItem.textContent = data[i].html_url;
+  listItem.textContent = data.name;
+  listItem.dataset.coord = JSON.stringify(data.coord);
+  cityList.appendChild(listItem);
+  //}
+}
+searchButton.addEventListener("click", getApi);
+//"cityList.addEventListener("click");
+//Save city name to local storage in button format + recall function
+
+// present current weather in the middle page
+
+//Present 5-day forcast
